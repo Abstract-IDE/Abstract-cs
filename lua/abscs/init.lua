e337358adfe77327898a34dc4fb87a3901a33369
Ali Shahid <canttell@gmail>
2022-06-20 13:32:05 +0545

    [rename] in favour of new name

--- lua/abscs/init.lua
+++ lua/abscs/init.lua
@@ -2,6 +2,6 @@ local util          = require 'abscs.util'
 local theme         = require 'abscs.theme'
 
 vim.o.background    = 'dark'
-vim.g.colors_name   = 'abscs-cs'
+vim.g.colors_name   = 'abscs'
 
 util.load(theme)
