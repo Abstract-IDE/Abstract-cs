e91e00708186c83121709c85f791e1b151d5f9cf
Ali Shahid <shaeinst@gmail.com>
2024-01-21 11:20:21 +0545

    [guisp] add support for guisp

--- lua/abscs/util.lua
+++ lua/abscs/util.lua
@@ -1,19 +1,20 @@
 local M = {}
 
 local function highlight(group, properties)
-  local bg = properties.bg == nil and '' or 'guibg=' .. properties.bg
-  local fg = properties.fg == nil and '' or 'guifg=' .. properties.fg
-  local style = properties.style == nil and '' or 'gui=' .. properties.style
-  local cmd = table.concat({ 'highlight', group, bg, fg, style }, ' ')
-  vim.api.nvim_command(cmd)
+	local bg = properties.bg == nil and "" or "guibg=" .. properties.bg
+	local fg = properties.fg == nil and "" or "guifg=" .. properties.fg
+	local guisp = properties.guisp == nil and "" or "guisp=" .. properties.guisp
+	local style = properties.style == nil and "" or "gui=" .. properties.style
+	local cmd = table.concat({ "highlight", group, bg, fg, guisp, style }, " ")
+	vim.api.nvim_command(cmd)
 end
 
 function M.load(theme)
-  for _, v in pairs(theme) do
-    for group, properties in pairs(v) do
-      highlight(group, properties)
-    end
-  end
+	for _, v in pairs(theme) do
+		for group, properties in pairs(v) do
+			highlight(group, properties)
+		end
+	end
 end
 
 return M
