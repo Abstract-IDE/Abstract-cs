27f06dae6bedd05347e1a1fa4b866155a3593cd8
Ali Shahid <shaeinst@gmail.com>
2024-08-09 01:53:20 +0400

    [improve] batter now

--- lua/abstract_cs/util.lua
+++ lua/abstract_cs/util.lua
@@ -3,6 +3,10 @@ local M = {}
 ---@param name string
 ---@param val vim.api.keyset.highlight
 M.set_highlight = function(name, val)
+	-- these two lines are taken from neovim's source code
+	val.force = true         -- Force links
+	val.cterm = val.cterm or {} --  Make sure that `cterm` attribute is not populated from `gui`
+
 	vim.api.nvim_set_hl(0, name, val)
 end
 
