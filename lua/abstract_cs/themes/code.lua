3ec32c2135d3fe323d43298ac09ad2e3dbe0828f
Ali Shahid <code@AlixShahid.com>
2025-07-29 02:02:49 +0400

    [theme/code] make indent color little more dark

--- lua/abstract_cs/themes/code.lua
+++ lua/abstract_cs/themes/code.lua
@@ -11,7 +11,7 @@ return {
     warn            = "#8e906b",
     border          = "#888888",
     highlight       = "#74adc7",
-    indent          = "#212121",
+    indent          = "#1a1a1a",
     indent_scope    = "#3F3F3F",
     sucess          = "#8FD5A5",
     error           = "#E06C75",
